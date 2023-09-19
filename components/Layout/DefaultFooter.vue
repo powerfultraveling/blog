2a8b827053f4bfaa68f26fbc2ab059b2f935d567
powerfultraveling <gastyylion@gmail.com>
2023-09-19 17:01:43 +0800

    add images and footer

---
+++ components/Layout/DefaultFooter.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="w-full bg-primary">
+    <div class="footer-wrapper container">
+      <div>
+        <a v-for="({ image, link }, i) in links" :key="i" :href="link">
+          <img :src="image" />
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import instagram from '~/assets/images/instagram.svg'
+import github from '~/assets/images/github.svg'
+import medium from '~/assets/images/medium.svg'
+
+const links = [
+  {
+    image: github,
+    link: ''
+  },
+  {
+    image: instagram,
+    link: ''
+  },
+  {
+    image: medium,
+    link: ''
+  }
+]
+</script>
+
+<style scoped>
+.footer-wrapper {
+  @apply flex justify-between items-end;
+}
+</style>
