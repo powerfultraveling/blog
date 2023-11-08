b765ec5f9ea376264a3f12a5494fa19539a6e7ea
powerfultraveling <gastyylion@gmail.com>
2023-11-08 15:39:54 +0800

    modified content and add english

--- components/Page/Home/HomeProjects.vue
+++ components/Page/Home/HomeProjects.vue
@@ -1,5 +1,5 @@
 <template>
-  <HomeSwiper :slides="slides" title="專案" />
+  <HomeSwiper :slides="slides" :title="$t('專案')" />
 </template>
 
 <script lang="ts" setup>
@@ -13,19 +13,21 @@ const { t } = useI18n()
 const slides = [
   {
     image: flyingV,
-    date: t('2022.03.20'),
+    date: '2022.03.20',
     label: t('flyingV 十週年網站'),
     link: 'https://exhibition.flyingv.cc/',
     content: t(
-      '慶祝 flying v 的活動網站，因為是活動網站，整體加上了許多比較酷炫的動畫效果，是一個比較活潑的網站。'
+      '這個專案是短期的活動網站，因此我們能夠展現創意為網站，增加了許多引人注目的視覺效果，使其更加生動活潑。'
     )
   },
   {
     image: kita,
-    date: t('2022.03.20'),
+    date: '2022.03.20',
     label: t('台灣友善動物協會'),
     link: 'https://www.kitanimals.org/',
-    content: t('協助友善動物協會重新開發官網，因為有大量的文章，所以採用 cms 的架構。')
+    content: t(
+      '這個專案的特點是有大量架構類似的文章需要呈現。為了更有效地處理這些內容，我們採用了 CMS 的架構。這使我們能夠更容易地管理、維護和更新網站的內容。'
+    )
   },
   {
     image: poya,
@@ -33,7 +35,7 @@ const slides = [
     label: t('寶雅官網'),
     link: 'https://www.poya.com.tw/',
     content: t(
-      '幫助重新開發寶雅官網，因為此專案規模比較大，主要困難點在最一開始的結構設計與工作的分配，如何在初期建立好共用元件與工具，讓每個工程師的任務不互相耦合是最重要的。'
+      '我負責寶雅官網的前端開發，這是一個規模較大的項目。在這個項目中，最大的挑戰在於一開始的結構設計和工作分配。我們需要確保在專案初期建立共用元件和適當的架構，以確保每位工程師的任務不會互相耦合，確保了專案的高效執行和代碼的可維護性。'
     )
   },
   {
@@ -42,7 +44,7 @@ const slides = [
     label: t('高鐵假期'),
     link: 'https://tholiday.thsrc.com.tw/?dialog=8b4e84b973d76a0367d240eac61bcc6f',
     content: t(
-      '主要經手業務邏輯的修改，由其商品內頁因業務邏輯複雜並互相牽連，因此，如何將 code 重構成易讀易修改的架構與 syntax 是本專案最重要的部分。'
+      '在這個專案中，我主要負責修改業務邏輯，尤其是商品內頁，因為業務邏輯相當複雜且相互關聯，因此，如何將代碼重構成容易閱讀且易於修改的架構和語法，確保代碼的維護性和擴展性是最重要的關鍵。'
     )
   }
 ]
