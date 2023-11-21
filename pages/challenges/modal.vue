589730f2d7386fb99bca0c4b2d177cdb88827ca9
powerfultraveling <gastyylion@gmail.com>
2023-11-21 19:26:36 +0800

    modified isOpened logic

--- pages/challenges/modal.vue
+++ pages/challenges/modal.vue
@@ -1,19 +1,9 @@
 <template>
   <!-- TODO: 這邊是測試用，之後要修掉 -->
   <div class="container">
-    {{ openedList }}
-    <button class="btn" @click="isOpened = true">click to open</button>
-    <button @click="$modal.open('hallo')">modal test</button>
-    <button class="btn" @click="$modal.close()">close</button>
-    <FModal :is-opened="isOpened" @close="isOpened = false" />
+    <button class="btn" @click="$modal.open('test')">open</button>
+    <FModal name="test" />
   </div>
 </template>
 
-<script lang="ts" setup>
-import { storeToRefs } from 'pinia'
-import { useModalStore } from '~/store/modal'
-
-const { openedList } = storeToRefs(useModalStore())
-
-const isOpened = ref(false)
-</script>
+<script lang="ts" setup></script>
