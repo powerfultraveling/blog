eeb4f7905fbd2833eb4134bc2b68a76c852d5993
powerfultraveling <gastyylion@gmail.com>
2023-11-27 15:40:31 +0800

    Updates

--- assets/PLink.vue.277cf335.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./nuxt-link.419c08b3.js";import{f as p,k as r,o as _,h as l,w as u,l as m,i as f,E as i}from"./entry.6417275e.js";const x=p({__name:"PLink",props:{to:{}},setup(o){const t=o,{$localePath:e}=i(),n=r(()=>e(t.to));return(s,d)=>{const a=c;return _(),l(a,{to:f(n)},{default:u(()=>[m(s.$slots,"default")]),_:3},8,["to"])}}});export{x as _};
