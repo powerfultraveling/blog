d25c8aa01211499cb30431f8a8eb37ef8640dae0
powerfultraveling <gastyylion@gmail.com>
2023-11-29 18:17:22 +0800

    add switch button; add cover up button

--- pages/challenges/buttons.vue
+++ pages/challenges/buttons.vue
@@ -1,5 +1,36 @@
 <template>
-  <div></div>
+  <ChallengeInnerPageWrapper title="各式按鈕">
+    <template #right>
+      <div class="space-y-4">
+        <FeatureItemCard :title="buttonInfo[ButtonType.SWITCH].title">
+          <div>
+            <PSwitch :active="active" @change="active = !active" />
+          </div>
+        </FeatureItemCard>
+        <FeatureItemCard :title="buttonInfo[ButtonType.COVER].title">
+          <div>
+            <CoverButton />
+          </div>
+        </FeatureItemCard>
+      </div>
+    </template>
+  </ChallengeInnerPageWrapper>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+enum ButtonType {
+  SWITCH = 'SWITCH',
+  COVER = 'COVER'
+}
+
+const buttonInfo = {
+  [ButtonType.SWITCH]: {
+    title: 'Toggler'
+  },
+  [ButtonType.COVER]: {
+    title: 'Cover Up'
+  }
+}
+
+const active = ref(false)
+</script>
