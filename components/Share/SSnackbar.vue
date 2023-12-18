d99e74bcdcc35da2216d71f6fac4959a83be7888
powerfultraveling <gastyylion@gmail.com>
2023-12-18 20:16:57 +0800

    add snack bar

---
+++ components/Share/SSnackbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  content: string
+  type: string
+}
+
+defineProps<Props>()
+
+const emits = defineEmits(['change'])
+
+function toggleActive() {
+  emits('change')
+}
+</script>
+
+<style scoped></style>
