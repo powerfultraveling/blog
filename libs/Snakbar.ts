a2289c9389e264b63b0d43207607cc4bbd62f247
powerfultraveling <gastyylion@gmail.com>
2023-12-19 16:41:16 +0800

    modified snackbar methods; add logic to show and hide

--- libs/Snakbar.ts
+++ libs/Snakbar.ts
@@ -1,4 +1,6 @@
 // show, hid, opened
+import { createVNode, render } from 'vue'
+import SSnackbar from '~/components/Share/SSnackbar.vue'
 
 type MessageType = 'success' | 'danger' | 'warn'
 
@@ -8,17 +10,41 @@ interface Message {
 }
 
 class Snackbar {
+  private opened: unknown[] = []
+
   constructor() {
     this.opened = []
   }
 
-  show() {}
+  show(message: Message) {
+    const container = document.createElement('div')
+
+    const vnode = createVNode(SSnackbar, {
+      ...message,
+      onHide: () => {
+        alert('hide')
+        document.querySelector('body')?.removeChild(container)
+        this.opened.splice(this.opened.indexOf(vm), 1)
+      }
+    })
+
+    render(vnode, container)
+    document.querySelector('body')?.appendChild(container)
+
+    const vm = vnode.component
+
+    this.opened = [...this.opened, vm]
+
+    return vm
+  }
 
   danger() {}
 
   success() {}
 
-  hide() {}
+  hide() {
+    this.opened.forEach((element) => element.exposed.hide())
+  }
 }
 
 export default Snackbar
