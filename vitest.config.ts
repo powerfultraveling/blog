459273c244daa38407d84437e833852f9688242f
powerfultraveling <gastyylion@gmail.com>
2024-01-03 18:34:26 +0800

    removed useless codes; add explain

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,11 @@
 import path from 'path'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
-// import Vue from '@vitejs/plugin-vue'
 
 export default defineVitestConfig({
-  // plugins: [Vue()],
   test: {
-    // could access the methods of vitest without import
+    // EXPLAIN: could access the methods of vitest without import
     globals: true,
+    // EXPLAIN: Using Nuxt could make vitest recognize composables from Nuxt
     environment: 'nuxt'
   },
   // enable Test file to import module by using absolute path
