695b7a2cd4752a7dac977085ab903ccc57dde6ca
powerfultraveling <gastyylion@gmail.com>
2024-01-09 18:05:35 +0800

    Updates

---
+++ assets/PLink.vue.35c1ef98.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./nuxt-link.d64df2d2.js";import{f as p,k as r,o as _,h as l,w as u,l as m,i as f,K as i}from"./entry.ae37be0e.js";const x=p({__name:"PLink",props:{to:{}},setup(o){const t=o,{$localePath:e}=i(),n=r(()=>e(t.to));return(s,d)=>{const a=c;return _(),l(a,{to:f(n)},{default:u(()=>[m(s.$slots,"default")]),_:3},8,["to"])}}});export{x as _};
--- assets/PLink.vue.ba4a90e8.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./nuxt-link.e6d27dec.js";import{f as p,k as r,o as _,h as l,w as u,l as m,i as f,K as i}from"./entry.e6c01bc3.js";const x=p({__name:"PLink",props:{to:{}},setup(o){const t=o,{$localePath:e}=i(),n=r(()=>e(t.to));return(s,d)=>{const a=c;return _(),l(a,{to:f(n)},{default:u(()=>[m(s.$slots,"default")]),_:3},8,["to"])}}});export{x as _};
