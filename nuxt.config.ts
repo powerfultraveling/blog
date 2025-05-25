879490e568965db48fb5f916bc7e5d040e76a2e4
powerfultraveling <gastbyylion@gmail.com>
2025-05-25 13:20:54 +0800

    modiifed nuxt config for dev server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,9 @@ import i18nConfig from './config/i18n'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'cloudflare'
-  },
-  app: {
-    baseURL: '/',
-    buildAssetsDir: 'assets'
-  },
+  // nitro: {
+  //   preset: 'cloudflare'
+  // },
 
   devtools: { enabled: true },
 
