59a5522754064fddb27b74d37978077190ed1f1e
powerfultraveling <gastbyylion@gmail.com>
2025-06-15 17:05:51 +0800

    added about content of myself

---
+++ libs/about/main.ts
@@ -0,0 +1,5 @@
+import { ABOUT_PROGRAMMING } from '@/libs/about/programming'
+import { ABOUT_LISTEN_MUSIC } from '@/libs/about/music'
+import { ABOUT_LANGUAGES } from '@/libs/about/languages'
+
+export const ABOUT_MAIN = [ABOUT_PROGRAMMING, ABOUT_LISTEN_MUSIC, ABOUT_LANGUAGES]
---
+++ libs/about/music.ts
@@ -0,0 +1,8 @@
+export const ABOUT_LISTEN_MUSIC = {
+  title: '關於聽音樂',
+  items: [
+    '喜歡邊散步邊聽音樂',
+    '比較常聽的音樂種類是 Jazz Fusion、古典音樂、配樂、City Pop、英文老歌',
+    '在工作時喜歡用有線耳機，不怕沒連上藍牙音樂播出來的尷尬'
+  ]
+}
