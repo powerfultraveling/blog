34d14cddf42b7fa2a8fb9dc3d3859ae81afc9c8d
powerfultraveling <gastbyylion@gmail.com>
2025-06-29 16:58:12 +0800

    added basic page

---
+++ components/Page/Post/LinkListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="link-list-item">
+    <div class="underline-word">
+      {{ title }}
+    </div>
+    <CaretStroke />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  title: string
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped>
+.link-list-item {
+  @apply p-8 text-2xl flex justify-between items-center space-x-4 xl:text-5xl;
+
+  &:hover {
+    .underline-word::after {
+      @apply w-full;
+    }
+
+    :deep(.caret),
+    :deep(.caret-stroke) {
+      @apply bg-gray;
+    }
+  }
+}
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="pt-10">
     <PageTitleWithLayout title="文章列表" />
-    <div class="container">
-      <!-- <ListLink
+
+    <!-- <div class="container">
+      <ListLink
         v-for="{ title, link } in articleList"
         :key="link"
         :title="title"
         :to="link"
         is-external
-      /> -->
-    </div>
+      />
+    </div> -->
   </div>
 </template>
 
-<!-- <script lang="ts" setup>
-import { articleList } from '~/libs/article/articleList'
-</script> -->
+<script lang="ts" setup>
+// const articleList = [
+//   {
+//     link: '/posts/1',
+//     title: '文章1'
+//   },
+//   {
+//     link: '/posts/2',
+//     title: '文章2'
+//   }
+// ]
+</script>
 
 <style scoped>
 .list-link {
