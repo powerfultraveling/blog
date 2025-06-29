a49e676c74ae629acfd7583c1ae96eff5f12ad1b
powerfultraveling <gastbyylion@gmail.com>
2025-06-29 16:53:31 +0800

    added post page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <!-- <HomeAbout /> -->
-    <HomeHero />
-    <!-- <HomeProjects /> -->
+  <div class="pt-10">
+    <PageTitleWithLayout title="文章列表" />
+    <div class="container">
+      <!-- <ListLink
+        v-for="{ title, link } in articleList"
+        :key="link"
+        :title="title"
+        :to="link"
+        is-external
+      /> -->
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-// const { data } = await useAsyncData('articles', () => getArticles())
-</script>
+<!-- <script lang="ts" setup>
+import { articleList } from '~/libs/article/articleList'
+</script> -->
+
+<style scoped>
+.list-link {
+  &:nth-child(odd) {
+    @apply bg-white;
+  }
+}
+</style>
